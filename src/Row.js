1c5c44898f59cfb96371732308ce9d9a9d3926e0
Brandon Nguyen <brannguyen12345@berkeley.edu>
2022-11-11 20:17:13 -0800

    add slider function

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React from "react";
+/** FaReg = unlike FaHeart = like */
+import { FaHeart, FaRegHeart, FaArrowLeft,FaArrowRight} from "react-icons/fa";
 export default function Row(props) {
+    const [like,setLike] = React.useState(false)
     const [movies,setMovies] = React.useState([])
     React.useEffect(async() => {
         const response = await fetch(props.fetchData)
@@ -8,21 +11,31 @@ export default function Row(props) {
             setMovies(data.results)
         }
     },[props.fetchData])
-    console.log(movies)
+    function handleLike() {
+        return(
+        setLike(prev => !prev)
+        )
+    }
     return (
         <div className="genreSec">
             <h1 className="genreTitle">{props.title}</h1>
             <div className="movie-section">
-                {movies.map(movie => (
-                    <>
-                        <div className="movieBoxes">
-                            <img className = "movieImg"src={`https://image.tmdb.org/t/p/w500/${movie? movie.backdrop_path: undefined}`}></img>
-                            <div class="middle">
-                                <div class="text">{movie.title}</div>
-                            </div> 
-                        </div>
-                    </>
-                ))}
+                <FaArrowLeft className="leftIcon"></FaArrowLeft>
+                <div id={"slider"} className ="slider">
+                    {movies.map(movie => (
+                        <>
+                            <div className="movieBoxes">
+                                <img className = "movieImg"src={`https://image.tmdb.org/t/p/w500/${movie? movie.backdrop_path: undefined}`}></img>
+                                <div class="middle">
+                                    <div class="text">{movie.title}</div>
+                                    {like == false && <p className="likeBut" onClick={handleLike}><FaRegHeart></FaRegHeart></p>}
+                                    {like == true && <p className="likeBut" onClick={handleLike}><FaHeart></FaHeart></p>}
+                                </div> 
+                            </div>
+                        </>
+                    ))}
+                </div>
+                <FaArrowRight className="rightIcon"></FaArrowRight>
             </div>
         </div>
     )
