51b9f69e3ce5baf36d5727513697a54fb5bb107d
Brandon Nguyen <brannguyen12345@berkeley.edu>
2022-11-15 19:02:37 -0800

    firebase portion

--- src/context/Login.jsx
+++ src/context/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Login = () => {
     /**Reload screen to display NavBar */
     (function()
@@ -17,7 +17,27 @@ const Login = () => {
     })();
     return (
     <div>
-
+      <div>
+        <div className='signInPage'>
+          <div className='blackBackground'>
+              <form>              
+                <h1>Sign In</h1>
+                <input className='emailInput' placeholder='Email or phone number'></input>
+                <input className='passwordInput' placeholder='Password'></input>
+                <button type='submit' className='submitButton'>Sign In</button>
+                <div className='remHelp'>
+                  <p><input type="checkbox" className='remember'></input>Remember me</p>
+                  <p className='help'>Need Help?</p>
+                </div>
+                <div className='subscr-in'>
+                  <p className='already-sub'>New to Netflix?</p>
+                  <Link className= 'signLink'to ='Signup'><p className='signButton'>Sign Up</p></Link>
+                </div>
+              </form>
+            </div>
+            <img src='https://user-images.githubusercontent.com/16425113/129554147-6ac7ba51-43e7-4c8e-ba77-e646a3ef6b12.jpg'></img>
+        </div>
+      </div>
     </div>
   )
 }
--- src/context/Signup.jsx
+++ src/context/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navibar from '../Navibar'
+import {Link} from 'react-router-dom'
 const Signup = () => {
       /**Reload screen to display NavBar */
       (function()
@@ -15,9 +16,29 @@ const Signup = () => {
             localStorage.removeItem('firstLoad');
         }
       })();
+
     return (
-    <div>
-    </div>
+      <div>
+        <div className='signUpPage'>
+          <div className='blackBackground'>
+              <form>              
+                <h1>Sign Up</h1>
+                <input className='emailInput' placeholder='Email or phone number'></input>
+                <input className='passwordInput' placeholder='Password'></input>
+                <button type='submit' className='submitButton'>Sign Up</button>
+                <div className='remHelp'>
+                  <p><input type="checkbox" className='remember'></input>Remember me</p>
+                  <p className='help'>Need Help?</p>
+                </div>
+                <div className='subscr'>
+                  <p className='already-sub'>Already subscribed to Netflix?</p>
+                  <Link className= 'signLink'to ='Login'><p className='signButton'>Sign In</p></Link>
+                </div>
+              </form>
+            </div>
+            <img src='https://user-images.githubusercontent.com/16425113/129554147-6ac7ba51-43e7-4c8e-ba77-e646a3ef6b12.jpg'></img>
+        </div>
+      </div>
   )
 }
 
